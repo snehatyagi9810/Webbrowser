f205b8342429917f901e34a1f3716b7df37b2249
Sneha Tyagi <snehtyagi9810@gmail.com>
2023-12-04 18:59:56 +0530

    Initial Commit

---
+++ Webbrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Webbrowser
+//
+//  Created by Sneh Tyagi on 04/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
